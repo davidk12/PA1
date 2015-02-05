73315544155ffc6236b8881703d72c0807a974fb
olingi <regnbogasulta@gmail.com>
2015-02-05 13:27:34 +0000

    bætti við smá í main til að velja algorithm frá command line

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,22 @@ public class Main {
 	public static void main(String[] args){
 		try{
 			// TODO: put in your agent here
-			Agent agent = new RandomAgent();
+			Agent agent = new PA1Agent();
 
 			int port=4001;
-			if(args.length>=1){
-				port=Integer.parseInt(args[0]);
+            String algorithm;
+			if(args.length>=2){
+				port = Integer.parseInt(args[0]);
+                algorithm = args[1].toUpperCase();
+                if (!algorithm.equals("DFS") || !algorithm.equals("BFS") || !algorithm.equals("UNI")){
+                    System.out.println("Algorithm not supported");
+                    System.out.println("Supported Algorithms: DFS, BFS, UNI");
+                    System.exit(0);
+                }
+                System.out.println("Using: " + algorithm + " algorithm");
+                // TODO: Use the algorithm
 			}
-			GamePlayer gp=new GamePlayer(port, agent);
+            GamePlayer gp=new GamePlayer(port, agent);
 			gp.waitForExit();
 		}catch(Exception ex){
 			ex.printStackTrace();
